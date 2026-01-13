8db5efdfa22a1c49f676a1d4e76b8ade44789e6e
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2026-01-13 18:46:14 +0530

    fixed production server errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utility";
 import { connectToDB } from "@/lib/mongoose";
 import Product from "@/lib/models/product.model";
-import { scrapeAmazonProduct } from "@/lib/scraper";
+import { scrapeProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 export const maxDuration = 60; // This function can run for a maximum of 300 seconds
